1b160b1614ac2f169061371b33eac6f421133cff
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2025-06-20 01:46:32 +0400

    efae

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,26 +1,10 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-import ProductList from "@/components/ProductList";
-import products from "../../data/products.json";
+import { Suspense } from "react";
+import SearchComponent from "@/components/SearchComponent";
 
 export default function SearchPage() {
-  const searchParams = useSearchParams();
-  const query = searchParams?.get("query") || "";
-
-  function handleAddToCart(product: any) {
-    alert(`Added "${product.name}" to cart!`);
-  }
-
-  const filtered = products.filter(product =>
-    product.name.toLowerCase().includes(query.toLowerCase())
-  );
-
   return (
-    <div className="max-w-6xl mx-auto py-10 px-4">
-      <h1 className="text-2xl font-bold mb-6">
-        Search results for: <span className="text-chocolate">{query}</span>
-      </h1>
-      <ProductList products={filtered} onAddToCart={handleAddToCart} />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchComponent />
+    </Suspense>
   );
 }
