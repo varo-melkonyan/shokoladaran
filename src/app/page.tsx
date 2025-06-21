8132ad97448f7f782fe514bd68032642be8a0f49
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2025-06-22 00:29:04 +0400

    шш

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,28 @@ import SectionGrid from "@/components/SectionGrid.client";
 import bestSellers from "@/data/bestSellers.json";
 import newProducts from "@/data/newsProducts.json";
 
-// async function fetchExclusivesProducts() {
-//   let baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-//   let res;
-//   try {
-//     res = await fetch(`${baseUrl}/api/admin/exclusives-products`, { cache: "no-store" });
-//     if (!res.ok) throw new Error();
-//   } catch {
-//     baseUrl = "http://localhost:3000";
-//     res = await fetch(`${baseUrl}/api/admin/exclusives-products`, { cache: "no-store" });
-//     if (!res.ok) throw new Error("Failed to fetch exclusives products");
-//   }
-//   return res.json();
-// }
+async function fetchExclusivesProducts() {
+  let baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  let res;
+  try {
+    res = await fetch(`${baseUrl}/api/admin/exclusives-products`, { cache: "no-store" });
+    if (!res.ok) throw new Error();
+  } catch {
+    baseUrl = "http://localhost:3000";
+    res = await fetch(`${baseUrl}/api/admin/exclusives-products`, { cache: "no-store" });
+    if (!res.ok) throw new Error("Failed to fetch exclusives products");
+  }
+  return res.json();
+}
 
 export default async function HomePage() {
-  // const exclusivesProducts = await fetchExclusivesProducts();
+  const exclusivesProducts = await fetchExclusivesProducts();
   return (
     <>
       <SectionHero />
       <SectionGrid title="Best Sellers" items={bestSellers} />
       <SectionGrid title="News" items={newProducts} />
-      {/* <SectionGrid title="Exclusives" items={exclusivesProducts} /> */}
+      <SectionGrid title="Exclusives" items={exclusivesProducts} />
     </>
   );
 }
