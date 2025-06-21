cc68c7b8eb5da814b91f54c90d55e34cb4304438
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2025-06-21 19:09:46 +0400

    fwa

--- src/pages/api/admin/exclusives-products.ts
+++ src/pages/api/admin/exclusives-products.ts
@@ -1,36 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs";
-import path from "path";
 
-const filePath = path.join(process.cwd(), "src/data/exclusivesProducts.json");
-
-function readData() {
-  if (!fs.existsSync(filePath)) return [];
-  return JSON.parse(fs.readFileSync(filePath, "utf-8"));
-}
-
-function writeData(data: any) {
-  // fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-}
+let exclusivesProducts: any[] = [];
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    const data = readData();
-    res.status(200).json(data);
+    res.status(200).json(exclusivesProducts);
   } else if (req.method === "POST") {
-    const data = readData();
-    data.push(req.body);
-    writeData(data);
+    exclusivesProducts.push(req.body);
     res.status(200).json({ success: true });
   } else if (req.method === "PUT") {
-    writeData(req.body.exclusivesProducts);
+    exclusivesProducts = req.body.exclusivesProducts;
     res.status(200).json({ success: true });
   } else if (req.method === "DELETE") {
-    const data = readData();
     const { index } = req.query;
     if (typeof index === "string") {
-      data.splice(Number(index), 1);
-      writeData(data);
+      exclusivesProducts.splice(Number(index), 1);
     }
     res.status(200).json({ success: true });
   } else {
