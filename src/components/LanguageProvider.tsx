22c18c6145e90a1431544de7ca9c9c7ee13ebd4a
Varo Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2025-08-30 09:20:13 +0400

    bug fix

--- src/components/LanguageProvider.tsx
+++ src/components/LanguageProvider.tsx
@@ -1,14 +1,27 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import i18n from "@/i18n";
+import { useTranslation } from "react-i18next";
 
-export default function LanguageProvider({ children }: { children: React.ReactNode }) {
+export default function LanguageProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { i18n } = useTranslation();
+  const [isClient, setIsClient] = useState(false);
+
   useEffect(() => {
+    setIsClient(true);
     const savedLng = localStorage.getItem("lng");
     if (savedLng && i18n.language !== savedLng) {
       i18n.changeLanguage(savedLng);
     }
-  }, []);
+  }, [i18n]);
+
+  if (!isClient) {
+    return null;
+  }
 
   return <>{children}</>;
 }
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,32 +1,31 @@
-// i18n.ts
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "@/data/locales/en.json";
 import ru from "@/data/locales/ru.json";
 import hy from "@/data/locales/hy.json";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-const getInitialLanguage = () => {
-  if (typeof window !== "undefined") {
-    const savedLng = localStorage.getItem("lng");
-    if (savedLng) return savedLng;
-  }
-  return "hy";
-};
 
 i18n
   .use(initReactI18next)
+  .use(LanguageDetector) 
   .init({
     resources: {
       en: { translation: en },
       ru: { translation: ru },
       hy: { translation: hy },
     },
-    lng: getInitialLanguage(),
+
+    lng: "hy",
     fallbackLng: "hy",
     interpolation: { escapeValue: false },
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
   });
 
 export default i18n;
 
-export const locales = ['en', 'hy', 'ru'] as const;
-export const defaultLocale = 'hy';
+export const locales = ["en", "hy", "ru"] as const;
+export const defaultLocale = "hy";
